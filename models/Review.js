640d4b14013d9bc878dba45fabb144354dc23989
Joseph Knopke <josephknopke@gmail.com>
2021-08-23 14:33:47 -0500

    more renderReviews method

--- models/Review.js
+++ models/Review.js
@@ -20,12 +20,38 @@ class Review {
             let h3 = document.createElement("h3")
             let p = document.createElement("p")
             let likeButton = document.createElement('button')
+            let ul = document.createElementNS("ul")
+
+            let reviewComments = review.comments.map(comment => {
+                let li = document.createElement("li")
+                let div = document.createElement("div")
+                let commentContent = document.createElement("p")
+                let commentLikes = document.createElement("p")
+                let likeButton = document.createElement("button")
+                commentContent.innerText = comment.content
+                commentLikes.innerText = comment.likes
+                likeButton.innerText = "❣"
+                div.appendChild(commentContent)
+                div.appendChild(commentLikes)
+                div.appendChild(likeButton)
+                li.appendChild(div)
+                return li
+            })
 
             div.id = review.id
             h3.innerText = review.title
             p.innerText = review.content
             likeButton.innerText = "❣"
-            likeButton.addEventListener('click', likeReview)
+            likeButton.addEventListener('click', likeReview.bind(review))
+
+            div.appendChild(h3)
+            div.appendChild(p)
+            div.appendChild(likeButton)
+            reviewComments.forEach(li => ul.appendChild(li))
+            div.appendChild(ul)
+
+            reviewsContainer().appendChild(div)
+
         })
     }
 }
