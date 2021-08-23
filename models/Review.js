5134dab492fe573474a5dd5c73a847db8febd6a8
Joseph Knopke <josephknopke@gmail.com>
2021-08-23 18:29:05 -0500

    like functionality

--- models/Review.js
+++ models/Review.js
@@ -14,6 +14,55 @@ class Review {
         .then(json => Review.renderReviews(json))
     }
 
+
+    static renderReviews(reviewsInfo){
+        clearContainer(reviewsContainer())
+        reviewsInfo.forEach(review => {
+            let div = document.createElement("div")
+            let h4 = document.createElement("h4")
+            let p = document.createElement("p")
+            let likeButton = document.createElement('button')
+            let ul = document.createElement("ul")
+            let likeCounter = document.createElement("p")
+
+            let reviewComments = review.comments.map(comment => {
+                let li = document.createElement("li")
+                let div = document.createElement("div")
+                let commentContent = document.createElement("p")
+                let commentLikes = document.createElement("p")
+                let likeButton = document.createElement("button")
+                likeCounter.innerText = review.likes
+                commentContent.innerText = comment.content
+                commentLikes.innerText = comment.likes
+                likeButton.innerText = "❣"
+                div.appendChild(commentContent)
+                div.appendChild(commentLikes)
+                div.appendChild(likeButton)
+                li.appendChild(div)
+                return li
+            })
+
+            div.id = review.id
+            div.style.padding = "20px"
+            div.className = "card"
+            h4.innerText = review.title
+            p.innerText = review.content
+
+            likeButton.innerText = "❣"
+            likeButton.addEventListener('click', Review.likeReview.bind(review))
+
+            div.appendChild(h4)
+            div.appendChild(p)
+            div.appendChild(likeCounter)
+            div.appendChild(likeButton)
+            reviewComments.forEach(li => ul.appendChild(li))
+            div.appendChild(ul)
+
+            reviewsContainer().appendChild(div)
+        
+        })
+    }
+
     static likeReview(e){
         this.likes += 1
 
@@ -37,46 +86,4 @@ class Review {
         .then(reviewsInfo => Review.renderReviews(reviewsInfo))
     }
 
-    static renderReviews(reviewsInfo){
-        reviewsInfo.forEach(review => {
-            let div = document.createElement("div")
-            let h4 = document.createElement("h4")
-            let p = document.createElement("p")
-            let likeButton = document.createElement('button')
-            let ul = document.createElementNS("ul")
-
-            let reviewComments = review.comments.map(comment => {
-                let li = document.createElement("li")
-                let div = document.createElement("div")
-                let commentContent = document.createElement("p")
-                let commentLikes = document.createElement("p")
-                let likeButton = document.createElement("button")
-                commentContent.innerText = comment.content
-                commentLikes.innerText = comment.likes
-                likeButton.innerText = "❣"
-                div.appendChild(commentContent)
-                div.appendChild(commentLikes)
-                div.appendChild(likeButton)
-                li.appendChild(div)
-                return li
-            })
-
-            div.id = review.id
-            div.style.padding = "20px"
-            div.className = "card"
-            h4.innerText = review.title
-            p.innerText = review.content
-            likeButton.innerText = "❣"
-            likeButton.addEventListener('click', Review.likeReview.bind(review))
-
-            div.appendChild(h4)
-            div.appendChild(p)
-            div.appendChild(likeButton)
-            reviewComments.forEach(li => ul.appendChild(li))
-            div.appendChild(ul)
-
-            reviewsContainer().appendChild(div)
-        
-        })
-    }
 }
