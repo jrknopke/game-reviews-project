30fe4a353d91723e38e5a87d3c303614ad55f578
Joseph Knopke <josephknopke@gmail.com>
2021-08-23 14:03:19 -0500

    classes

---
+++ models/Comment.js
@@ -0,0 +1,8 @@
+class Comment {
+    constructor(id, content, likes, review_id){
+        this.id = id
+        this.content = content
+        this.likes = likes
+        this.review_id = review_id
+    }
+}
--- models/Review.js
+++ models/Review.js
@@ -1,3 +1,12 @@
 class Review {
 
+    constructor(id, title, content, likes, comments){
+        this.id = id
+        this.title = title
+        this.content = content
+        this.likes = likes
+        this.comments = [...comments]
+        
+
+    }
 }
